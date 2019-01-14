b3fa33e7289d41d6168ab30f9a998fa5e077a59a
liskova <liskova.misa@post.cz>
2019-01-14 22:50:03 +0100

    slunce a les už tam jsou, třídy ještě ne.

--- woods.py
+++ woods.py
@@ -14,22 +14,29 @@ def change_bg_b(t):
 
 def change_bg_a(t):
     background.image = background_picture_a
-   background.draw()
+    background.draw()
     pyglet.clock.schedule_once(change_bg_b, 4)
 
 
+class Hidden:
+    def __init__(self, name):
+        print("vytvářím {}".format(name))
+        self.name = name
+
+
+# window for the game
 window = pyglet.window.Window()
 
+# loads images for background
 background_picture_a = pyglet.image.load("bg_01_a.jpg")
 background_picture_b = pyglet.image.load("bg_01_b.jpg")
 background = pyglet.sprite.Sprite(background_picture_a)
 
-
+# start background changing
 pyglet.clock.schedule_once(change_bg_b, 2)
 
 window.push_handlers(
     on_show=draw_background,
-    )
-
+)
 
 pyglet.app.run()
